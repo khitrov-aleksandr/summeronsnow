1ee242bd5d5aecedea643ec626ce8ebb682eb534
Хитров Александр <khitrov.av@erkapharm.com>
2021-10-03 20:45:20 +0300

    Первая страница

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <title>Hello, world!</title>
+</head>
+<body class="bg-yellow-800">
+<div class="container mx-auto">
+    <div class="flex h-screen">
+        <div class="m-auto">
+            <img class="w-11/12 m-auto border-4 border-yellow-700" src="/storage/images/autumn_low.jpg"/>
+        </div>
+    </div>
+</div>
+<script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
